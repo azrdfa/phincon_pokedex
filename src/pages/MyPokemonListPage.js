6bcefe021432eebef9bb94281328ef9b4197afec
azrdfa <aazhard8@gmail.com>
2021-08-15 19:26:49 +0700

    Add-feature: Finish MyPokemonPage functionality

--- src/pages/MyPokemonListPage.js
+++ src/pages/MyPokemonListPage.js
@@ -1,18 +1,31 @@
 import { MyPokemonContext } from '../contexts/MyPokemonContext'
+import { useContext } from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const MyPokemonListPage = () => {
+  const {myPokemon, removePokemon} = useContext(MyPokemonContext)
   console.log("Render MyPokemonListPage")
-    return (
-        <MyPokemonContext.Consumer>
-          {(context) => {
-            return (
-              <>
-                <h1>MyPokemonListPage</h1>
-              </>
-            )
-          }}
-        </MyPokemonContext.Consumer>
-    )
+  return (
+    <>
+      {
+        myPokemon.map(elem => {
+          return <Card key={elem.nickname} style={{margin: "1rem"}}>
+            <Card.Body style={{display: "flex", justifyContent: "space-between"}}>
+              <div>
+                <Card.Title>Nickname: {elem.nickname}</Card.Title>
+                <Card.Subtitle>Pokemon: {elem.name}</Card.Subtitle>
+              </div>
+              <Button
+                variant="secondary"
+                onClick={() => {removePokemon(elem.nickname)}}
+              >Release</Button>
+            </Card.Body>
+          </Card>
+        })
+      }
+    </>
+  )
 }
 
 export default MyPokemonListPage
